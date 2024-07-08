07aa1d975d4ce1a19632ca177a9155b2e922fa47
ancestralmiel <julian.gomez@mx.lactalis.com>
2024-07-07 22:04:13 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import spacy
+import sqlite3
+import pyodbc
+
+
+st.title('PLATAFORMA DE VALIDACION👈')
+
+st.write("______________________________________")
+
+
+st.header(" DEVOLUCIONES🤑")
+read= pd.read_excel('C:/Users/manitas/Desktop/DEVOLUCION.xlsx')
+st.dataframe(read)
+
+
+
+
+st.write("______________________________________")
+
+
+st.header(" LACTASALES🥛")
+
+read= pd.read_excel('C:/Users/manitas/Desktop/LACTASALES.xlsx')
+st.dataframe(read)
